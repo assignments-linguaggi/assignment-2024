a4b1e4b1750fb8f58681feae3ba692d7beeab25b
SimoneMenozzi <114143017+SimoneMenozzi@users.noreply.github.com>
2024-05-17 22:22:49 +0200

    Update LoopLICM.cpp
    cambiato i criteri per la code motion.

--- TerzoAssignment/TestMenozzi/LoopLICM.cpp
+++ TerzoAssignment/TestMenozzi/LoopLICM.cpp
@@ -85,7 +85,7 @@ void LoopLICM::applyCodeMotion() {
             }
         }
 
-        /*bool dominatesUsers = true;
+        bool dominatesUsers = true;
         for (User *user : instr->users()) {
             if (Instruction *userInstr = dyn_cast<Instruction>(user)) {
                 BasicBlock *userBlock = userInstr->getParent();
@@ -94,7 +94,7 @@ void LoopLICM::applyCodeMotion() {
                     break;
                 }
             }
-        }*/
+        }
 
         bool isDeadAtExit = false;
         // Verifica se la variabile definita dall'istruzione è "dead" all'uscita del loop
@@ -110,7 +110,7 @@ void LoopLICM::applyCodeMotion() {
         }
 
         //if ((assignsToUnassigned && dominatesExits && dominatesUsers) || isDeadAtExit) {
-            if (assignsToUnassigned && (dominatesExits || isDeadAtExit)) {
+            if (assignsToUnassigned && (dominatesExits || isDeadAtExit)  && dominatesUsers ) {
             instr->moveBefore(preheader->getTerminator());
             movedInstructions.insert(instr);
         }
